8af8012790dbca62d2785b15625386f30e95e10a
Kenneth Ballenegger <kenneth@ballenegger.com>
2014-06-25 16:38:11 -0700

    comment app style / functionality w/ bootstrap

--- src/js/comment-app.jsx
+++ src/js/comment-app.jsx
@@ -1,29 +1,23 @@
 /** @jsx React.DOM */
 
-var converter = {
-        'makeHtml': function (x) {
-            return x + ' - BLAH';
-        }
-    };
-var mdToHtml = function mdToHtml(md) {
-        return converter.makeHtml(md);
-    };
+var Markdown = require('markdown-js').markdown;
 
+var Panel = Bootstrap.Panel;
+var Button = Bootstrap.Button;
 
 var Comment = React.createClass({
     render: function () {
-        return <div className="comment">
-            <div className="author">{this.props.author} -- {this.props.username}</div>
-            <div className="text">{mdToHtml(this.props.text)}</div>
-        </div>;
+        return <Panel header={this.props.prefix + this.props.author}>
+            <div dangerouslySetInnerHTML={{__html: Markdown(this.props.text)}}></div>
+        </Panel>;
     }
 });
 
 var CommentList = React.createClass({
     render: function () {
-        var username = this.props.username;
+        var prefix = this.props.prefix;
         var comments = this.props.data.map(function (e) {
-            return <Comment author={e.author} text={e.text} key={e.id} username={username} />
+            return <Comment author={e.author} text={e.text} key={e.id} prefix={prefix} />
         });
         return <div>{comments}</div>;
     }
@@ -31,11 +25,17 @@ var CommentList = React.createClass({
 
 var CommentInput = React.createClass({
     render: function () {
-        return <form onSubmit={this.submit}>
-            <input type="text" placeholder="Name" ref="author" />
-            <input type="text" placeholder="Say something..." ref="text" />
-            <input type="submit" value="Post" />
-        </form>;
+        return Bootstrap.Panel({}, <form onSubmit={this.submit}>
+            <div className="form-group">
+                <input type="text" className="form-control" placeholder="Name" ref="author" />
+            </div>
+            <div className="form-group">
+                <textarea className="form-control" placeholder="Say something..." ref="text"></textarea>
+            </div>
+            <div className="form-group">
+                <input type="submit" className="btn btn-primary" value="Post" />
+            </div>
+        </form>);
     },
     submit: function () {
         var authorDom = this.refs.author.getDOMNode();
@@ -52,14 +52,16 @@ var CommentInput = React.createClass({
 });
 
 var counter = 3;
-
 var CommentBox = module.exports = React.createClass({
     render: function () {
-        return <div className="comment-box">
-            <input type="text" value={this.state.username}
-                onChange={this.usernameChanged} ref="username" />
+        return <div>
+            <form className="form-inline"><div className="form-group">
+                    <label className="control-label" style={{'padding-right': '10px'}}>Prefix: </label>
+                    <input className="form-control" type="text" value={this.state.prefix}
+                        onChange={this.prefixChanged} ref="prefix" />
+            </div></form>
             <h1>Comments:</h1>
-            <CommentList data={this.state.data} username={this.state.username} />
+            <CommentList data={this.state.data} prefix={this.state.prefix} />
             <CommentInput callback={this.commentsUpdated} />
         </div>;
     },
@@ -68,18 +70,18 @@ var CommentBox = module.exports = React.createClass({
             data: [
                 {id: 1, author: "Brandon Goldman", text: "I am Brandon!"},
                 {id: 2, author: "George Burke", text: "I am George!"},
-                {id: 3, author: "Kenneth Ballenegger", text: "I am Kenneth!"}
+                {id: 3, author: "Kenneth Ballenegger", text: "I am **Kenneth**!"}
             ],
-            username: "Kenneth"
+            prefix: "Author: "
         };
     },
-    usernameChanged: function () {
+    prefixChanged: function () {
         var state = this.state;
-        state["username"] = this.refs.username.getDOMNode().value;
+        state["prefix"] = this.refs.prefix.getDOMNode().value;
         this.setState(state);
     },
     commentsUpdated: function (comment) {
-        comment[id] = ++counter;
+        comment.id = ++counter;
         this.setState({data: this.state.data.concat(comment)});
     }
 });
